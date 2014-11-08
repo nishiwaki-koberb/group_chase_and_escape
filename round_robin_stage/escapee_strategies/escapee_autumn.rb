92b3f1b2396620d148624c168e1e79b9836e74f9
akiro harada <akiro.harada@gmail.com>
2014-11-08 16:07:04 +0900

    今回変更したのは鬼が近いときだけ動かすところ

--- round_robin_stage/escapee_strategies/escapee_autumn.rb
+++ round_robin_stage/escapee_strategies/escapee_autumn.rb
@@ -1,20 +1,27 @@
 require 'pp'
 
 class EscapeeStrategy
+  
+  def initialize
+  end
 
   def next_direction(chaser_positions, escapee_positions)
-    dx, dy = chaser_positions.first
-    candidate = [[1,0],[-1,0],[0,1],[0,-1]]
-    if dx > 0
-      candidate.delete([1,0])
-    elsif dx < 0
-      candidate.delete([-1,0])
+    points = { [1,0] => 0.0, [-1,0] => 0.0, [0,1] => 0.0, [0,-1] => 0.0 }
+    chaser_positions[0..5].each do |dx,dy|
+      r = (dx.abs + dy.abs).to_f
+      if dx > 0 && dx < 5
+        points[ [-1,0] ] += dx.abs/(r*r)
+      elsif dx < 0 && dx > -5
+        points[ [1,0] ] += dx.abs/(r*r)
+      end
+      if dy > 0 && dy < 5
+        points[ [0,-1] ] += dy.abs/(r*r)
+      elsif dy < 0 && dy > -5
+        points[ [0,1] ] += dy.abs/(r*r)
+      end
     end
-    if dy > 0
-      candidate.delete([0,1])
-    elsif dy < 0
-      candidate.delete([0,-1])
-    end
-    candidate.sample
+    direction = points.max {|a,b| a[1] <=> b[1] }[0]
+    direction
   end
 end
+  
