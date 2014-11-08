b33f7d70c2b7da02172e52b178d326bb8e044861
s.tokunaga <otokuna46@gmail.com>
2014-11-08 14:37:14 +0900

    change the chaser strategy

--- round_robin_stage/chaser_strategies/chaser-tokunaga.rb
+++ round_robin_stage/chaser_strategies/chaser-tokunaga.rb
@@ -1,15 +1,22 @@
 require 'pp'
 
 class ChaserStrategy
-
   def initialize
+    @stage_number = 0
+    @direction_history=[]
   end
 
   def next_direction(chaser_positions, escapee_positions)
+    @stage_number = @stage_number +1
+    #if @stage_number > 1000
+    #  exit
+    #end
     candidate = []
     return [0,0] if escapee_positions.empty?
     dx, dy = escapee_positions.first
+
     r = 5.0 / (dx.abs + dy.abs)
+
     if rand < r
       if dx > 0
         candidate.push [1,0]
@@ -24,6 +31,14 @@ class ChaserStrategy
     else
       candidate = [[1,0],[-1,0],[0,1],[0,-1]]
     end
-    candidate.sample
+
+    @direction_history.push(candidate.sample)
+    p @direction_history
+    
+    if escapee_positions.size < 3
+       candidate.min 
+    else
+      candidate.sample
+    end
   end
 end
