f869edb26deb11aaebc372b910202d0b3022842e
Tatsuya Ogi <t.oginogin@gmail.com>
2014-11-08 15:53:06 +0900

    update chaser logic

--- round_robin_stage/chaser_strategies/chaser_ogi.rb
+++ round_robin_stage/chaser_strategies/chaser_ogi.rb
@@ -2,9 +2,10 @@ require 'pp'
 
 class ChaserStrategy
   @@chaser_count = 0
-  @@zenigata1_id = [1]
-  @@zenigata2_id = [2]
-  @@step_limit = 10
+  @@zenigata_id = [1,2]
+  @@zenigata2_id = [3]
+  @@step_limit = 50
+  @@position_limit = 25
 
   attr_accessor :id, :from_initial_position, :limit, :step
 
@@ -12,48 +13,49 @@ class ChaserStrategy
     @@chaser_count = @@chaser_count + 1
     @id = @@chaser_count
     @from_initial_position = [0,0]
-    @limit = 25
     @step = 0
   end
 
   def next_direction(chaser_positions, escapee_positions)
     return [0,0] if escapee_positions.empty?
-    return escapee_position escapee_positions.first if @@zenigata1_id.include? @id
-    return escapee_position escapee_positions.first if @@zenigata2_id.include? @id
+    return closer_position escapee_positions.first if @@zenigata_id.include? @id
 
-    if step >= @@step_limit
+    @step = @step + 1
+    if @step >= @@step_limit
+      @step = 0
       escapee_id = 0
       escapee_id = 1 if escapee_positions.count >= 2
-      return escapee_position escapee_positions[escapee_id]
+      # return closer_position escapee_positions.sample if @@zenigata2_id.include? @id
+      return closer_position escapee_positions.first
     end
 
     escapee_positions.each do |escapee|
-      pos = escapee_position escapee
+      pos = closer_position escapee
 
-       if escapee_positions.count < 5
-       end 
-        if (@from_initial_position[0] + pos[0]).abs <= @limit + @id
-          @from_initial_position[0] = @from_initial_position[0] + pos[0]
-        else
-          next
-        end
+      new_x = @from_initial_position[0] + pos[0]
+      if new_x.abs <= @@position_limit + @id
+        @from_initial_position[0] = new_x
+      else
+        next
+      end
 
-        if (@from_initial_position[1] + pos[1]).abs <= @limit + @id
-          @from_initial_position[1] = @from_initial_position[1] + pos[1]
-        else
-          next
-        end
+      new_y = @from_initial_position[1] + pos[1]
+      if new_y.abs <= @@position_limit + @id
+        @from_initial_position[1] = @from_initial_position[1] + pos[1]
+      else
+        next
+      end
 
-        return pos
-      # end
-
+      return pos
     end
 
     return [0,0]
   end
 end
 
-def escapee_position(escapee)
+private
+
+def closer_position(escapee)
   dx, dy = escapee
   candidate = []
   if dx > 0
