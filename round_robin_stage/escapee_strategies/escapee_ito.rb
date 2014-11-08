ce83c06c3cc7b05133dd0b54d7ab0c3c9d129ef6
Junichi Ito <jit@sonicgarden.jp>
2014-11-08 13:10:30 +0900

    Create escape logic.

--- round_robin_stage/escapee_strategies/escapee_ito.rb
+++ round_robin_stage/escapee_strategies/escapee_ito.rb
@@ -1,20 +1,24 @@
 require 'pp'
 
 class EscapeeStrategy
+  def initialize
+    @direction = [[1,0],[-1,0],[0,1],[0,-1]].sample
+  end
 
   def next_direction(chaser_positions, escapee_positions)
-    dx, dy = chaser_positions.first
-    candidate = [[1,0],[-1,0],[0,1],[0,-1]]
-    if dx > 0
-      candidate.delete([1,0])
-    elsif dx < 0
-      candidate.delete([-1,0])
-    end
-    if dy > 0
-      candidate.delete([0,1])
-    elsif dy < 0
-      candidate.delete([0,-1])
-    end
-    candidate.sample
+    @direction
+    # dx, dy = chaser_positions.first
+    # candidate = [[1,0],[-1,0],[0,1],[0,-1]]
+    # if dx > 0
+    #   candidate.delete([1,0])
+    # elsif dx < 0
+    #   candidate.delete([-1,0])
+    # end
+    # if dy > 0
+    #   candidate.delete([0,1])
+    # elsif dy < 0
+    #   candidate.delete([0,-1])
+    # end
+    # candidate.sample
   end
 end
